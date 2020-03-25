2d34742e42111c933760f269bb827f606973d740
Nikita P. Shupeyko <webyneter@gmail.com>
2020-03-25 14:54:12 +0300

    Update copyright notice

--- docker-aliases.plugin.zsh
+++ docker-aliases.plugin.zsh
@@ -1,6 +1,6 @@
 # The MIT License (MIT)
 
-# Copyright (c) 2017 Nikita P. Shupeyko
+# Copyright (c) 2017-2020 Nikita P. Shupeyko
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
